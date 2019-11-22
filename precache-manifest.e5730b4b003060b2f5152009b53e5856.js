4947491d2d6e05db55ea97ef0f1c395466fd7409
mattlistor <mattlistor@gmail.com>
2019-11-21 21:47:23 -0500

    Updates

---
+++ precache-manifest.e5730b4b003060b2f5152009b53e5856.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47550361623e300b42556d9875c355c3",
+    "url": "/rubikscube/index.html"
+  },
+  {
+    "revision": "fa49bcb841b4fe221de6",
+    "url": "/rubikscube/static/css/main.4aa81a18.chunk.css"
+  },
+  {
+    "revision": "d5187f9d929aa8851b77",
+    "url": "/rubikscube/static/js/2.81d6ac9a.chunk.js"
+  },
+  {
+    "revision": "fa49bcb841b4fe221de6",
+    "url": "/rubikscube/static/js/main.a64e7cb7.chunk.js"
+  },
+  {
+    "revision": "75692863a47c10e674ad",
+    "url": "/rubikscube/static/js/runtime-main.5150a666.js"
+  }
+]);
